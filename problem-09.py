25e3b88a51220fc86590193afb6322920a6d2475
jakedmtr <jakedmtr@gmail.com>
2023-09-27 19:35:45 +0100

    solved problem-09

--- problem-09.py
+++ problem-09.py
@@ -8,13 +8,22 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
 
-"""
-c^2 = a^2 + b^2
-c = sqrt(a^2 + b^2)
-a = sqrt(c^2 - b^2)
-b = sqrt(c^2 - a^2)
+from math import sqrt
 
-a + b + c = 1000 = sqrt(c^2 - b^2) + sqrt(c^2 - a^2) + sqrt(a^2 + b^2)
-"""
+a = 1
+exit_status = 0  # for exiting while loop because i'm too stupid to figure out another way
 
-impossible
+while a < 997:
+    if exit_status == 1:
+        break
+    for b in range(2, 998):
+        c = sqrt((a**2)+(b**2))
+        triplet = [a, b, c]
+        if sqrt((a**2)+(b**2)) == (1000 - a - b):
+            print(triplet[0] * triplet[1] * triplet[2])  # prints product from the 3 numbers in the triplet list
+            exit_status = 1
+            break
+        b += 1
+    a += 1
+   
+
